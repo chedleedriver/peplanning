a98e2fd02c05828edd6a5a0c7308ffdb9abef7ac
steve <steve@steve.itfacilitas.co.uk>
2013-06-13 15:35:04 +0100

    pdf output

--- public/tplan/code/config.php
+++ public/tplan/code/config.php
@@ -1,8 +1,8 @@
 <?
 defined('FPDF_FONTPATH')
-    || define('FPDF_FONTPATH',realpath(dirname(__FILE__) . '/tplan/font/'));
+    || define('FPDF_FONTPATH',realpath(dirname(__FILE__) . '/../public/tplan/font/'));
 defined('IMAGE_PATH')
-    || define ('IMAGE_PATH',realpath(dirname(__FILE__) . '/tplan/images/'));
+    || define ('IMAGE_PATH',realpath(dirname(__FILE__) . '/../public/tplan/images/'));
 defined('DIAGRAM_PATH')
     || define ('DIAGRAM_PATH','diagrams/');
 defined('RESOURCE_PATH')
@@ -10,7 +10,7 @@ defined('RESOURCE_PATH')
 defined('DOC_ROOT')
     || define ('DOC_ROOT','/tplan/');
 defined('FILE_ROOT')
-    || define ('FILE_ROOT',realpath(dirname(__FILE__) . '/tplan/'));
+    || define ('FILE_ROOT',realpath(dirname(__FILE__) . '/../public/tplan/'));
 defined('HOME_URL')
     || define ('HOME_URL',$_SERVER['SERVER_NAME']);
 $tp = mysql_pconnect("peplanning-db.my.phpcloud.com", "peplanning", "ferd1nand") or trigger_error(mysql_error(),E_USER_ERROR);
