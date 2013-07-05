381550eed19fc4cc23a9e0424690095c859865a9
steve <steve@steve.itfacilitas.co.uk>
2013-07-05 12:25:18 +0100

    Changed database connection details

--- public/tplan/Connections/tp.php
+++ public/tplan/Connections/tp.php
@@ -3,8 +3,8 @@
 # Type="MYSQL"
 # HTTP="true"
 $hostname_tp = "localhost";
-$database_tp = "tplan";
-$username_tp = "tp";
-$password_tp = "enquiry";
+$database_tp = "peplanning";
+$username_tp = "peplanning";
+$password_tp = "ferd1nand";
 $tp = mysql_pconnect($hostname_tp, $username_tp, $password_tp) or trigger_error(mysql_error(),E_USER_ERROR); 
 ?>
