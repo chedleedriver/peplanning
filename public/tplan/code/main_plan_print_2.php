072bacd71a0e2ade741df293d32e28635d277380
steve@itfacilitas.co.uk <>
2013-09-04 11:45:22 +0000

    print changes

--- public/tplan/code/main_plan_print_2.php
+++ public/tplan/code/main_plan_print_2.php
@@ -611,7 +611,7 @@ fclose($pdfIn);
 // Clean up after ourselves
 $htmlFile=realpath(dirname(__FILE__)).'/../tmp/'.$file.'.html';
 $pdfFile=realpath(dirname(__FILE__)).'/../tmp/'.$file.'.pdf';
-$success=passthru(escapeshellcmd(realpath(dirname(__FILE__)) . "/../wkhtmltopdf -l -O landscape -s A4 -T 2mm -B 5mm -R 2mm -L 2mm --footer-center [page] --footer-font-size 8 $htmlFile $pdfFile"));
+$success=passthru(escapeshellcmd("/usr/local/bin/wkhtmltopdf -l -O landscape -s A4 -T 2mm -B 5mm -R 2mm -L 2mm --footer-center [page] --footer-font-size 8 $htmlFile $pdfFile"));
 $browsePDF=fopen($pdfFile,'r');
 $pdf_name=time().".pdf";
 /**header("Content-type:application/pdf");
