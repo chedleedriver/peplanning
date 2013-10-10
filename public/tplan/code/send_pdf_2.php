ccb5bc519c14542c1fcedc0dc2199ad242dc9b0e
steve@itfacilitas.co.uk <>
2013-10-10 15:05:16 +0000

    assessment pdf

--- public/tplan/code/send_pdf_2.php
+++ public/tplan/code/send_pdf_2.php
@@ -2,13 +2,13 @@
 include('lessonfunctions_2.php');
 $orientation=$_GET['orientation'];
 $file=$_GET['file'];
-$htmlFile=realpath(dirname(__FILE__)).'/../tmp/'.$file.'.html';
-$pdfFile=realpath(dirname(__FILE__)).'/../tmp/'.$file.'.pdf';
+$htmlFile=FILE_ROOT.'/tmp/'.$file.'.html';
+$pdfFile=FILE_ROOT.'/tmp/'.$file.'.pdf';
 $success=passthru(escapeshellcmd("/usr/local/bin/wkhtmltopdf -l -O $orientation -s A4 -T 2mm -B 2mm -R 2mm -L 2mm $htmlFile $pdfFile"));
-//$browsePDF=fopen($pdfFile,'r');
-//$pdf_name=time().".pdf";
-//header("Content-type:application/pdf");
-//header("Content-Disposition:inline;filename=$pdf_name");
-//readfile($pdfFile);
-echo '/tplan/tmp/'.$file.'.pdf'
+$browsePDF=fopen($pdfFile,'r');
+$pdf_name=time().".pdf";
+header("Content-type:application/pdf");
+header("Content-Disposition:inline;filename=$pdf_name");
+readfile($pdfFile);
+
 ?>
