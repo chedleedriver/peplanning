b1a4b58b7ffca6c5aced1d1597ce431e29d63a56
steve@itfacilitas.co.uk <>
2013-11-13 12:00:13 +0000

    really last one

--- application/controllers/AuthController.php
+++ application/controllers/AuthController.php
@@ -836,7 +836,7 @@ class AuthController extends Zend_Controller_Action
             {
               if($_GET['link']!='no')
                 {
-                  $this->_helper->redirector('usercheckAction');
+                  $this->_helper->redirector('usercheck');
                 }
               else 
                 {
