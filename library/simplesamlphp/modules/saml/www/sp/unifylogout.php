2ab96f401880924232c73fa7f9978893a09ad64c
Steve Priest <steve@itfacilitas.co.uk>
2013-11-28 14:50:17 +0000

    path problems

--- library/simplesamlphp/modules/saml/www/sp/unifylogout.php
+++ library/simplesamlphp/modules/saml/www/sp/unifylogout.php
@@ -2,7 +2,7 @@
 
 // Define path to application directory
 defined('APPLICATION_PATH')
-    || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/var/www/html/peplanning/application'));
+    || define('APPLICATION_PATH','/var/www/html/peplanning/application'));
 
 // Define application environment
 defined('APPLICATION_ENV')
@@ -28,7 +28,7 @@ require_once 'Zend/Application.php';
 // Create application, bootstrap, and run
 $application = new Zend_Application(
     APPLICATION_ENV,
-    APPLICATION_PATH . '/configs/application.ini'
+   '/var/www/html/peplanning/application/configs/application.ini'
 );
  $mysession = new Zend_Session_Namespace('mysession');
        $id=$mysession->id;
