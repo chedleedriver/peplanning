511fa4f5f0af22c39f4313fa288f7d40ccd87405
Steve Priest <steve@itfacilitas.co.uk>
2013-11-29 09:39:45 +0000

    removed DB entries from logout

--- library/simplesamlphp/modules/saml/www/sp/unifylogout.php
+++ library/simplesamlphp/modules/saml/www/sp/unifylogout.php
@@ -33,17 +33,16 @@ $application = new Zend_Application(
  $mysession = new Zend_Session_Namespace('mysession');
        $id=$mysession->id;
        $username=$mysession->username;
-       $users  = new Application_Model_DbTable_Users();
-       if ($mysession->unifyuser) $response['unify']=1; else $response['unify']=0;
        $this->_helper->layout()->disableLayout();
        $this->_helper->viewRenderer->setNoRender();
        Zend_Auth::getInstance()->clearIdentity();
        Zend_Session::destroy();
-       if(!Zend_Auth::getInstance()->hasIdentity())
+       /*if(!Zend_Auth::getInstance()->hasIdentity())
        {    
             $active_users = new Application_Model_DbTable_ActiveUsers;
             $active_row = $active_users->fetchRow("id = $id");
             if ($active_row) $deactive = $active_row->delete();
        }
+       */
 
 ?>
