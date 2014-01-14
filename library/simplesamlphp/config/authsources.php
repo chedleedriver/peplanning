3df869ce4060238a1674aed8e1395a454c0f85c9
Steve Priest <steve@itfacilitas.co.uk>
2014-01-14 12:22:20 +0000

    unify changes for the live site

--- library/simplesamlphp/config/authsources.php
+++ library/simplesamlphp/config/authsources.php
@@ -19,7 +19,7 @@ $config = array(
 		// The entity ID of this SP.
 		// Can be NULL/unset, in which case an entity ID is generated based on the metadata URL.
 		//'entityID' => 'https://peplanning-dev.itfacilitas.co.uk/simplesaml/module.php/saml/sp/metadata.php/rm',
-    'entityID' => 'https://dev.peplanning.org.uk/simplesaml/module.php/saml/sp/metadata.php/rm',
+    'entityID' => 'https://www.peplanning.org.uk/simplesaml/module.php/saml/sp/metadata.php/rm',
 
 		// The entity ID of the IdP this should SP should contact.
 		// Can be NULL/unset, in which case the user will be shown a list of available IdPs.
