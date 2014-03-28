6ea48be8f545ac7520e94f7c98fb54b2fafa3e72
Steve Priest <steve@itfacilitas.co.uk>
2014-03-28 14:56:08 +0000

    jujij

--- public/js/main.js
+++ public/js/main.js
@@ -91,7 +91,7 @@ function getJQLessons(getWhat,getDesc,getLevel,getTopic,getNumLessons,getScrollN
         });
     $(".item").css("color", "#808386");
     $("#myplans_item_"+getWhat).css("color", "#77AF0C");
-    var i = $("#lesson-list").scrollable();
+    var i = $("#lesson-list");
     var s = i.data("scrollable");
     s.seekTo(getScrollNum);
 }
