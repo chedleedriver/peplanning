6aefd1c1eec910586de1641953b39cd89caabc41
Steve Priest <steve@itfacilitas.co.uk>
2014-03-28 12:09:19 +0000

    go to current scroll item

--- public/js/main.js
+++ public/js/main.js
@@ -91,6 +91,9 @@ function getJQLessons(getWhat,getDesc,getLevel,getTopic,getNumLessons)
         });
     $(".item").css("color", "#808386");
     $("#myplans_item_"+getWhat).css("color", "#77AF0C");
+    var lesson_list = $('.vertical').data('scrollable');
+    var curr_lesson = lesson_list.getIndex();
+    lesson_list.seekTo(curr_lesson);
 }
 function processLessons(data)
 {
